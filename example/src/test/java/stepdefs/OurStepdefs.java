dcec761a448315de09546b8ea9c527a8d5c107ef
erfan <erfan@smartech.ir>
2025-05-01 17:37:05 +0330

    Add step definitions for Part 3 Calculator

---
+++ example/src/test/java/stepdefs/OurStepdefs.java
@@ -0,0 +1,88 @@
+package stepdefs;
+
+import calculator.OurCalculator;
+import io.cucumber.java.BeforeAll;
+import io.cucumber.java.en.Given;
+import org.junit.Assert;
+
+public class OurStepdefs {
+    private static OurCalculator calculator;
+    private int value1;
+    private int value2;
+    private float result;
+    private boolean exceptionThrown;
+
+    @BeforeAll
+    public static void setUp() {
+        calculator = new OurCalculator();
+    }
+
+    @Given("Two input numbers, {int} and {int}")
+    public void twoInputNumbersAnd(int int1, int int2) {
+        value1 = int1;
+        value2 = int2;
+    }
+
+    @Given("I add the two numbers")
+    public void iAddTheTwoNumbers() {
+        try {
+            result = calculator.add(value1, value2);
+            exceptionThrown = false;
+        } catch (ArithmeticException e) {
+            exceptionThrown = true;
+        }
+    }
+
+
+    @Given("I subtract the two numbers")
+    public void iSubtractTheTwoNumbers() {
+        try {
+            result = calculator.subtract(value1, value2);
+            exceptionThrown = false;
+        } catch (ArithmeticException e) {
+            exceptionThrown = true;
+        }
+    }
+
+    @Given("I multiply the two numbers")
+    public void iMultiplyTheTwoNumbers() {
+        try {
+            result = calculator.multiply(value1, value2);
+            exceptionThrown = false;
+        } catch (ArithmeticException e) {
+            exceptionThrown = true;
+        }
+    }
+
+    @Given("I divide the two numbers")
+    public void iDivideTheTwoNumbers() {
+        try {
+            result = calculator.divide(value1, value2);
+            exceptionThrown = false;
+        } catch (ArithmeticException e) {
+            exceptionThrown = true;
+        }
+    }
+
+    @Given("I raise the first number to the power of the second number")
+    public void iRaiseTheFirstNumberToThePowerOfTheSecondNumber() {
+        try {
+            result = calculator.power(value1, value2);
+            exceptionThrown = false;
+        } catch (ArithmeticException e) {
+            exceptionThrown = true;
+        }
+    }
+
+
+    @Given("the result should be {float}")
+    public void theResultShouldBe(float expected) {
+        Assert.assertEquals(expected, result, 0.001);
+        Assert.assertFalse(exceptionThrown);
+    }
+
+    @Given("the result should be an error")
+    public void theResultShouldBeAnError() {
+        Assert.assertTrue(exceptionThrown);
+    }
+}
